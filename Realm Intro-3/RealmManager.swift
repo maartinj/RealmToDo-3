63a75fe3437a755bbc63e12034ccaf5325720735
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-07-13 00:51:37 +0200

    feat: Renaming the default realm

---
+++ Realm Intro-3/RealmManager.swift
@@ -0,0 +1,36 @@
+//
+//  RealmManager.swift
+//  Realm Intro-2
+//
+//  Created by Stewart Lynch on 2022-03-08.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmManager: ObservableObject {
+    
+    private(set) var realm: Realm?
+    init(name: String) {
+        initializeSchema(name: name)
+    }
+    
+    func initializeSchema(name: String) {
+        let docDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let realmFileUrl = docDir.appendingPathComponent("\(name).realm")
+        let config = Realm.Configuration(fileURL: realmFileUrl, schemaVersion: 1) { migration, oldSchemaVersion in
+            if oldSchemaVersion < 1 {
+                migration.enumerateObjects(ofType: Country.className()) { _, newObject in
+                    newObject!["flag"] = "🏳️"
+                }
+            }
+        }
+        Realm.Configuration.defaultConfiguration = config
+        print(docDir.path)
+        do {
+            realm = try Realm()
+        } catch {
+            print("Error opening default realm", error)
+        }
+    }
+}
